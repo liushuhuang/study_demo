20cc433b41daa6ac5e3921bcd61a7d3536f76eb9
刘述煌 <liush_kjfwb@zjrcu.com>
2023-04-07 16:19:48 +0800

    拦截器

--- src/main/java/com/cn/liu/Interceptor/InterceptorConfig.java
+++ src/main/java/com/cn/liu/Interceptor/InterceptorConfig.java
@@ -1,7 +1,5 @@
 package com.cn.liu.Interceptor;
 
-import com.cn.liu.Interceptor.AuthInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
